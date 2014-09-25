36b3c08d75ca9c7450cbb9267c27da5b38843cd5
boostrack <hflambo@boostrack.com>
2014-09-25 23:10:31 +0100

    cleanup

--- run-demo.sh
+++ run-demo.sh
@@ -1,8 +1,125 @@
 #!/bin/sh
 
+## CORE
+
+NAME="boostrack-debian-nucleos-hystrix-server-ui"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3101:8080 -t boostrack/debian:nucleos-hystrix-server-ui
+
+
+NAME="boostrack-debian-nucleos-batch-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3102:9393 -t boostrack/debian:nucleos-batch-server
+
+
+NAME="boostrack-debian-nucleos-config-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3103:8080 -t boostrack/debian:nucleos-config-server
+
+
+NAME="boostrack-debian-nucleos-hystrix-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3104:8080 -t boostrack/debian:nucleos-hystrix-server
+
+
+NAME="boostrack-debian-nucleos-eureka-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3105:8080 -t boostrack/debian:nucleos-eureka-server
+
+
+
+## MICROSERVICES
+
+NAME="boostrack-debian-microservices-suppliers"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3106:8080 -t boostrack/debian:microservices-suppliers
+
+
+NAME="boostrack-debian-microservices-store"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3107:8080 -t boostrack/debian:microservices-store
+
+
+NAME="boostrack-debian-microservices-doge"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3108:8080 -t boostrack/debian:microservices-doge
+
+
+NAME="boostrack-debian-microservices-customers"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3109:8080 -t boostrack/debian:microservices-customers
+
+
+NAME="boostrack-debian-microservices-account"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3110:8080 -t boostrack/debian:microservices-account
+
+
+
+## IODENTITY
+
+NAME="boostrack-debian-identity-login-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3111:8080 -t boostrack/debian:identity-login-server
+
+
+NAME="boostrack-debian-identity-uaa-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3112:8080 -t boostrack/debian:identity-uaa-server
+
+
+
+## UI SITES
+
+NAME="boostrack-debian-mbit-site-server"
+
+IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
+
+docker run --name $NAME -d -p 3113:8080 -t boostrack/debian:mbit-site-server
+
+
 NAME="boostrack-debian-recomendation"
 
 IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
 [ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
 
-docker run --name $NAME -d -p 3333:8080 -t boostrack/debian:recomendation
+docker run --name $NAME -d -p 3114:8080 -t boostrack/debian:recomendation
+
+
--- scripts/docker-cleanup.sh
+++ scripts/docker-cleanup.sh
@@ -4,10 +4,10 @@
 # See http://docs.docker.io/en/latest/use/workingwithrepository/
 
 # delete all non-running container
-docker ps -a | grep 'Exit' |  awk '{print $1}' | xargs docker rm > /dev/null &
+docker ps -a | grep 'Exit' |  awk '{print $1}' | xargs docker rm -f > /dev/null &
 
 # delete unused images
-docker images | grep '<none>' |  awk '{print $3}'  | xargs docker rmi > /dev/null &
+docker images | grep '<none>' |  awk '{print $3}'  | xargs docker rmi -f > /dev/null &
 
 # delete specific images
 # docker rmi app/example
