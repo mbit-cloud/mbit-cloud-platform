36c376fc398f80f385246ae7aac970b9cded72d0
boostrack <hflambo@boostrack.com>
2014-09-25 23:30:33 +0100

    cleanup

--- run-demo.sh
+++ run-demo.sh
@@ -39,7 +39,7 @@ NAME="boostrack-debian-nucleos-eureka-server"
 IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
 [ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
 
-docker run --name $NAME -d -p 3105:8080 -t boostrack/debian:nucleos-eureka-server
+docker run --name $NAME -d -p 3105:8888 -t boostrack/debian:nucleos-eureka-server
 
 
 
@@ -82,11 +82,11 @@ NAME="boostrack-debian-microservices-account"
 IMAGE_ID=$(docker ps -a | grep $NAME | cut -f1 -d' ')
 [ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Container: [$NAME] removed!! IMAGE_ID: $IMAGE_ID"
 
-docker run --name $NAME -d -p 3110:8080 -t boostrack/debian:microservices-account
+docker run --name $NAME -d -p 3110:8761 -t boostrack/debian:microservices-account
 
 
 
-## IODENTITY
+## IDENTITY
 
 NAME="boostrack-debian-identity-login-server"
 
