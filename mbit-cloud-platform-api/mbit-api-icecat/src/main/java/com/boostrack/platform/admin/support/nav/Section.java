81710528791a46e50480c42d706525477911d61a
boostrack <hflambo@boostrack.com>
2014-09-25 23:10:46 +0100

    cleanup

--- mbit-cloud-platform-api/mbit-api-icecat/src/main/java/com/boostrack/platform/admin/support/nav/Section.java
+++
@@ -1,9 +0,0 @@
-package com.boostrack.platform.admin.support.nav;
-
-/**
- * Used in conjunction with {@link com.boostrack.platform.admin.support.nav.Navigation} to indicate which section
- * of the site-wide navigation is currently active and highlighted.
- */
-public enum Section {
-    DOCS, GUIDES, PROJECTS, BLOG, QUESTIONS
-}
--- mbit-cloud-platform-api/mbit-api-icecat/src/main/java/com/boostrack/platform/admin/team/support/TeamImporter.java
+++
@@ -1,7 +0,0 @@
-package com.boostrack.platform.admin.team.support;
-
-import org.springframework.social.github.api.GitHub;
-
-interface TeamImporter {
-    void importTeamMembers(GitHub gitHub);
-}
